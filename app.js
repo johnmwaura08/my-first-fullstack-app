9360651e1e46f7f8d3c33abf971bcda7e5b981c4
Bobby Rockers <brockers@bisonok.com>
2019-10-12 09:35:15 -0500

    Working hello world html page.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/', express.static('files'));
+
 app.listen(3000, function(){
     console.log("Express running on port: 3000");
 });
