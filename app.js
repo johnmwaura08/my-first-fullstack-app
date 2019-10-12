270fc60bec28c4bdde81da7a5e2c0a42d34d51cd
Bobby Rockers <brockers@bisonok.com>
2019-10-12 10:14:48 -0500

    First working route.

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
+// DB STUFF =================================
+mongoose.connect('mongodb://localhost/todo');
+
+var tasksSchema = mongoose.Schema({
+    task: String
+});
+
+var todo = mongoose.model('tasks', tasksSchema);
+var db = mongoose.connection;
+
+db.on('error', function(msg){
+    console.log("Mongoose Error db todo:" + msg);
+});
+
+db.once('open', function(){
+    console.log("Mongoose connected to todo");
+});
+
+// DB STUFF =================================
+
 app.use('/', express.static('files'));
+app.get('/todo/all', function(req,res){
+    todo.find({}).exec(function(err,data){
+        res.json(data);
+    });
+});
 
 app.listen(3000, function(){
     console.log("Express running on port: 3000");
